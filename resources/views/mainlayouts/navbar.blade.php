fa0936916282594128abdfdbfde59e358cafdfbd
tusarhaque <tusarhaque12@gmail.com>
2022-11-10 18:23:28 +0600

    first commit

--- resources/views/mainlayouts/navbar.blade.php
+++ resources/views/mainlayouts/navbar.blade.php
@@ -15,10 +15,10 @@
 				<ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu float-right">
 					<li class="active"><a href="index.html">Home</a></li>
 					
-					<li><a href="/services">Services</a></li>
+					<!-- <li><a href="/services">Services</a></li> -->
+					<li><a href="/usershomepage">User Panel</a></li>
 					<li><a href="/about">About</a></li>
-					<li><a href="/usershomepage">Users Panel</a></li>
-					<li><a href="/about">Agency Panel</a></li>
+					<!-- <li><a href="/about">Agency Panel</a></li> -->
 				
 					<li><a href="/contact">Contact Us</a></li>
 				</ul>
